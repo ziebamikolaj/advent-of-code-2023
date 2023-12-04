9b074e776dac410ecfec10781746845900cac233
ziebamikolaj <102910351+ziebamikolaj@users.noreply.github.com>
2023-12-04 18:23:24 +0100

    Update updateReadme.py

--- updateReadme.py
+++ updateReadme.py
@@ -3,13 +3,13 @@ import os
 readme_template = """
 # 🎄 Advent of Code Progress 🌟
 
-**Welcome to my Advent of Code repository!** Explore my journey through the Advent of Code challenges, a series of fun and engaging programming puzzles.
+**Welcome to my Advent of Code repository!** Dive into my coding adventures with these fun and challenging puzzles.
 
 ## 🚀 Progress Overview
 
-I have completed **{} out of 25** challenges! 🎉
+📊 Progress: **{} out of 25 challenges completed!**
 
-![Progress](https://progress-bar.dev/{}/{},title=Completed)
+![Progress](https://progress-bar.dev/{},scale=100,title=Completed,color=green)
 
 | Day | Status | Solution |
 |:---:|:------:|:--------:|
@@ -17,11 +17,11 @@ I have completed **{} out of 25** challenges! 🎉
 
 ## About Advent of Code
 
-[Advent of Code](https://adventofcode.com/) is an exciting series of Christmas-themed programming challenges. It's a fantastic way to improve coding skills and engage with the coding community.
+[Advent of Code](https://adventofcode.com/) is a delightful series of coding challenges released each December. It's a fantastic way to sharpen coding skills and celebrate the holiday season with the coding community.
 
 ## Connect with Me
 
-Stay updated with my coding adventures on [GitHub](https://github.com/ziebamikolaj). Happy coding!
+Follow my coding journey on [GitHub](https://github.com/ziebamikolaj). Happy coding!
 
 """
 
@@ -30,17 +30,14 @@ completed_count = 0
 progress_rows = []
 for i in range(1, 26):  # Assuming 25 days in Advent of Code
     day_file = f"Day{i}.cs"
-    if os.path.isfile(day_file):
-        status_icon = "🟢"
-        status_link = f"[View Solution](https://github.com/ziebamikolaj/advent-of-code/blob/main/{day_file})"
-        completed_count += 1
-    else:
-        status_icon = "🔴"
-        status_link = "Not Started"
+    file_exists = os.path.isfile(day_file)
+    status_badge = "![Completed](https://img.shields.io/badge/Day%20{}-Completed-green)".format(i) if file_exists else "![Incomplete](https://img.shields.io/badge/Day%20{}-Incomplete-red)".format(i)
+    solution_link = f"[🔗 Solution](https://github.com/ziebamikolaj/advent-of-code/blob/main/{day_file})" if file_exists else "⏳ In Progress"
     
-    progress_rows.append(f"| Day {i} | {status_icon} | {status_link} |")
+    progress_rows.append(f"| Day {i} | {status_badge} | {solution_link} |")
+    completed_count += int(file_exists)
 
 # Update the README
 with open("README.md", "w") as readme_file:
-    progress_percentage = (completed_count / 25) * 100
-    readme_file.write(readme_template.format(completed_count, progress_percentage, 100, "\n".join(progress_rows)))
+    progress_percentage = int((completed_count / 25) * 100)
+    readme_file.write(readme_template.format(completed_count, progress_percentage, "\n".join(progress_rows)))
