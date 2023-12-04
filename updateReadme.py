5bd30b256aa78957b168d1702959a4319d3ca4c4
ziebamikolaj <102910351+ziebamikolaj@users.noreply.github.com>
2023-12-04 18:18:05 +0100

    Update updateReadme.py

--- updateReadme.py
+++ updateReadme.py
@@ -1,23 +1,35 @@
 import os
 
 readme_template = """
-# Advent of Code Progress
+# 🎄 Advent of Code Progress 🌟
 
-My progress for Advent of Code:
+**Welcome to my Advent of Code repository!** Here you'll find my solutions to the annual Advent of Code programming challenges. Advent of Code is a series of small programming puzzles for a variety of skill sets and skill levels in any programming language you like.
 
+## 🚀 Progress Overview
+
+I have completed **{} out of 25** challenges so far!
+
+| Day | Status |
+|-----|--------|
 {}
+
+## Stay Tuned!
+
+For more updates, follow me on [GitHub](https://github.com/ziebamikolaj). Happy coding!
 
 """
 
 # Generate the progress checklist
-progress = []
+completed_count = 0
+progress_rows = []
 for i in range(1, 26):  # Assuming 25 days in Advent of Code
     day_file = f"Day{i}.cs"
     if os.path.isfile(day_file):
-        progress.append(f"- [x] Day {i}")
+        progress_rows.append(f"| Day {i} | ![Completed](https://img.shields.io/badge/-Completed-green) |")
+        completed_count += 1
     else:
-        progress.append(f"- [ ] Day {i}")
+        progress_rows.append(f"| Day {i} | ![Incomplete](https://img.shields.io/badge/-Incomplete-red) |")
 
 # Update the README
 with open("README.md", "w") as readme_file:
-    readme_file.write(readme_template.format("\n".join(progress)))
+    readme_file.write(readme_template.format(completed_count, "\n".join(progress_rows)))
